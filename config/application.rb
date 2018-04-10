ba815f72de8651ea44a2c160fc97a3df1d8df27f
github username <github email address>
2018-04-10 12:27:28 -0400

    adding edits to config to take out assets precompile and editing the load for javascript on view

--- config/application.rb
+++ config/application.rb
@@ -26,6 +26,5 @@ module RailsBootstrap
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-    config.assets.initialize_on_precompile = false
   end
 end
