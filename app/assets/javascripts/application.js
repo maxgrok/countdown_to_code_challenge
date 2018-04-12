a26ef4d4a5b1a626f0d0f56c86700c77cba2b6d2
github username <github email address>
2018-04-12 16:05:42 -0400

    adding edits to no longer require turbolinks

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,7 +13,19 @@
 //
 //= require jquery
 //= require rails-ujs
-//= require turbolinks
 //= require bootstrap-sprockets
 //= require_tree .
  
+ var clock;
+
+      $(document).ready(function() {
+
+        // Instantiate a counter
+        clock = new FlipClock($('.clock'), 300, {
+          clockFace: 'HourlyCounter',
+          autoStart: true,
+          countdown: true
+        });
+        
+      });
+      $(document).ready('turbolinks:load')
