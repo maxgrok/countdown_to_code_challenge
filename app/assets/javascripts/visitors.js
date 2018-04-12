2c783ff974534580c77d68e9bd9c0ff963e8362c
github username <github email address>
2018-04-12 16:47:04 -0400

    adding correct date time to countdown function

--- app/assets/javascripts/visitors.js
+++ app/assets/javascripts/visitors.js
@@ -1,9 +1,11 @@
 $(document).ready(function(){
-	var date = new Date(2018, 4, 13, 10);
-    var now = new Date(2018, 4, 12, 4);
+	var date = new Date("April 13, 2018 10:00:00");
+    var now = new Date();
+
     var diff = (date.getTime()/1000) - (now.getTime()/1000);
 
     var clock = $('.clock').FlipClock(diff,{
+    	autoStart: true,
         clockFace: 'HourlyCounter',
         countdown: true
     }); 
